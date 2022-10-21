7e674d81bc65182da2731d2be742016f41565206
Irina Nesterenko <inesterenko@roox.ru>
2022-10-21 13:57:42 +0200

    Added Binary Tree

---
+++ src/BinaryTreeSet/binaryTreeSet.test.ts
@@ -0,0 +1,25 @@
+import BinaryTree from './binaryTreeSet'
+
+const createTree = () => {
+  return new BinaryTree([50, 20, 70, 10, 30, 80, 55])
+}
+
+describe('Binary Tree', () => {
+
+  it('Should add keys and chech exist', () => {
+    const tree = new BinaryTree();
+
+    tree.add('name');
+    tree.add('age');
+
+    expect(tree.has('name')).toStrictEqual(true);
+  })
+
+  it('Should delete', () => {
+    const tree = new BinaryTree([50, 20, 70, 10, 30, 80, 55]);
+
+    expect(tree.has(20)).toStrictEqual(true);
+    // expect(tree.delete(30)).toStrictEqual(true);
+    // expect(tree.has(30)).toStrictEqual(false);
+  })
+})
